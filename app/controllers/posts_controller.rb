9a0f3211f3c5166b9646f98695ec7e8c9ed8593b
Daniel Rocha <danielrocha94@msn.com>
2025-04-09 03:55:11 +0100

    Add post on create using turbo_stream

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,11 +9,30 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def new_form
   end
 
+  def create
+    @post = current_user.posts.new(post_params)
+
+    respond_to do |format|
+      if @post.save
+        format.html {redirect_to my_questions_path }
+        format.turbo_stream {}
+      end
+    end
+  end
+
   def delete
+  end
+
+  private
+
+  def post_params
+    params.require(:post)
+      .permit(:title, :body)
   end
 end
