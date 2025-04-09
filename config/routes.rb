dbbb30043b33b13f8450e8c9fe6dfc49cbe0523c
Daniel Rocha <danielrocha94@msn.com>
2025-04-09 02:14:06 +0100

    Finish Header

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,9 @@ Rails.application.routes.draw do
 
   resources :posts
 
+  get :my_questions, to: "dashboard#my_questions"
+  get :my_answers, to: "dashboard#my_answers"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
